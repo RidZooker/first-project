e25fa976feb94525918afb47ca2eea9226f4c778
RidZooker <jall3r@yandex.ru>
2023-04-22 01:13:43 +0300

    v0.002

--- Practicum.java
+++ Practicum.java
@@ -42,6 +42,7 @@ public class Main {
             }
         }
     }
+	
     public static void printMenu() {
         System.out.println("Что вы хотите сделать? ");
         System.out.println("1 - Конвертировать валюту");
